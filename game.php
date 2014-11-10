eccba601afdc691165fd912c2bb18e0ffc067deb
MGbeenieboy <dsgf01@gmail.com>
2014-11-10 15:06:15 +0100

    game.php wird nur durchgeführt wenn eingeloggt
    - Der Inhalt von game.php wird nur angezeigt wenn man eingeloggt ist,
    sonst erscheint, wie vorhin auch schon, eine Fehlermeldung

--- game.php
+++ game.php
@@ -11,28 +11,28 @@
 		session_start();
 		if (isset($_SESSION["id"]))
 		{
-			echo "Willkommen im Spiel. Deine ID ist " . $_SESSION["id"];
+			echo "Willkommen im Spiel. Deine ID ist " . $_SESSION["id"] .
+			"</div>
+			<div id='menu2'>
+				LOL HALLO was geht bei euch sou?
+			</div>
+			<div id='content'>
+				<div id='dorf'>
+					Test
+				</div>
+				<div id='info'>
+					Test nochmal
+				</div>
+			</div>
+			<div id='footer'>
+				Copyright &copy; Marcel Gregoriadis
+			</div>";
 		}
 		else
 		{
 			echo "Du bist nicht eingeloggt, du Hund!";
 		}
 		?>
-	</div>
-	<div id="menu2">
-		LOL HALLO was geht bei euch sou?
-	</div>
-	<div id="content">
-		<div id="dorf">
-			Test
-		</div>
-		<div id="info">
-			Test nochmal
-		</div>
-	</div>
-	<div id="footer">
-		Copyright &copy; Marcel Gregoriadis
-	</div>
 </div>
 </body>
 </html>
