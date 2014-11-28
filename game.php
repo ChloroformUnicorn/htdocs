7025777428092e9766136ae3257fc6cb9d6118b7
MGbeenieboy <dsgf01@gmail.com>
2014-11-28 16:23:53 +0100

    Wir haben ein Game Layout!
    Das 3 Spalten Layout ist bereits angedeutet.

--- game.php
+++ game.php
@@ -1,38 +1,24 @@
 <html>
 <head>
 	<title>Spiel</title>
-	<link rel="stylesheet" type="text/css" href="style.css" charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="game.css" charset="utf-8" />
 	<meta charset="utf-8">
 </head>
 <body>
-<div id="container">
-	<div id="menu">
-		<?php
-		session_start();
-		if (isset($_SESSION["id"]))
-		{
-			echo "Willkommen im Spiel. Deine ID ist " . $_SESSION["id"] .
-			"</div>
-			<div id='menu2'>
-				LOL HALLO was geht bei euch sou?
-			</div>
-			<div id='content'>
-				<div id='dorf'>
-					Test
-				</div>
-				<div id='info'>
-					Test nochmal
-				</div>
-			</div>
-			<div id='footer'>
-				Copyright &copy; Marcel Gregoriadis
-			</div>";
-		}
-		else
-		{
-			echo "Du bist nicht eingeloggt, du Hund!";
-		}
-		?>
+
+<div class="outterWrapper">
+    <div class="innerWrapper">
+        <div id="sidebar">
+            Test
+        </div>
+        <div id="overview">
+            Test
+        </div>
+        <div id="village">
+            <img width=600px src="graphic/village.jpg">
+        </div>
+    </div>
 </div>
+
 </body>
 </html>
